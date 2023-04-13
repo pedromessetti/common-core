75ee622fd2e3b08ecd003a0c3c4ecab09cf61ee9
pedromessetti <pedromessetti@gmail.com>
2023-04-13 09:43:41 +0100

    add function

---
+++ libft/ft_memmove.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmessett <pmessett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 08:55:37 by pmessett          #+#    #+#             */
+/*   Updated: 2023/04/13 09:42:33 by pmessett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	dest = ft_memcpy(dest, src, n);
+	return (dest);
+}
+
+//lorem ipsum 

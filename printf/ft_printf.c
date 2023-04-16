9c8f7d82c329bc2efc3952d89c31e1ab44f21dc1
Pedro Messetti <105685220+pedromessetti@users.noreply.github.com>
2023-04-16 18:27:55 +0100

    instructions

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -14,5 +14,17 @@
 
 int ft_printf(const char *, ...)
 {
+/*
+make functions for all the following conversions
 
+%c Prints a single character. (Join with %% function)
+%s Prints a string (as defined by the common C convention).
+%p The void * pointer argument has to be printed in hexadecimal format.
+%d Prints a decimal number.(Join with %i and %u function)
+%i Prints an integer in base 10.
+%u Prints an unsigned decimal (base 10) number.
+%x Prints a number in hexadecimal (base 16) lowercase format. (#define 0123456789abcdef in .h and join with %X)
+%X Prints a number in hexadecimal (base 16) uppercase format.
+%% Prints a percent sign.
+*/
 }
